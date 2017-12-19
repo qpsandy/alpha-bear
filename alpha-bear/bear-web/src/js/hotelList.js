4e5edd101cd47d698bb2f4f44e363fb78c20d2bd
qpsandy <qpsandy@126.com>
2017-12-19 23:14:03 +0800

    hotel list page

--- alpha-bear/bear-web/src/js/hotelList.js
+++ alpha-bear/bear-web/src/js/hotelList.js
@@ -30,7 +30,6 @@ $(function() {
 						<th>更新时间</th>\
 					</tr>';
 		arrList.push(head);
-		//有多个应用时，必须列出这些 应用
 		for(var i=0; i<arr.length; i++){
 			//str += '<li><a data-href="'+arr[i].bizUrl+'" data-guid="'+arr[i].guid+'" href="javascript:;">'+arr[i].name+'</a></li>';
 			var str = '<tr>\
@@ -51,7 +50,44 @@ $(function() {
 	        slideSpeed: 600, // 缓动速度。单位毫秒
 	        jump: true, //是否支持跳转
 	        callback: function(page) { // 回调函数
-	            console.log(page);
+	            console.log("jump to page " + page);
+	            $.ajax({
+                    url: '/bear/service/hotel/page',
+                    headers: {
+                        'Authorization': localStorage.getItem('Authorization') || ''
+                      },
+                    type: 'post',
+                    dataType: 'json',
+                    data: '{"pageNo":'+ page + ',"pageSize":10}',
+                    contentType: 'application/json'
+                })
+                .done(function(data) {
+                    //console.log(data);
+                    arr = data.result;
+                    var arrList = [];
+                    var head = '<tr>\
+                                    <th>酒店名称</th><th>酒店房型</th>\
+                                    <th>酒店早餐</th><th>酒店库存</th>\
+                                    <th>酒店价格</th><th>酒店价格变化</th>\
+                                    <th>更新时间</th>\
+                                </tr>';
+                    arrList.push(head);
+                    //有多个应用时，必须列出这些 应用
+                    for(var i=0; i<arr.length; i++){
+                        //str += '<li><a data-href="'+arr[i].bizUrl+'" data-guid="'+arr[i].guid+'" href="javascript:;">'+arr[i].name+'</a></li>';
+                        var str = '<tr>\
+                                    <td>'+arr[i].hotelname+'</td><td>'+arr[i].fx+'</td>\
+                                    <td>'+arr[i].zc+'</td><td>'+arr[i].kc+'</td>\
+                                    <td>'+arr[i].price+'</td><td>'+arr[i].price+'</td>\
+                                    <td>'+arr[i].updatetime+'</td>\
+                                   </tr>'
+                        arrList.push(str);
+                    }
+                    $('#BMOMSWEB').html(arrList.join(''));
+                })
+                .fail(function() {
+                    //console.log("error");
+                });
 	        }
     	})
 	})
@@ -61,12 +97,9 @@ $(function() {
 	
 	
 	// 当点击"查询"
-	function searchHotel(){
+	searchHotel = function (){
 		var hotelname = $("#hotelname").val();
 		
-		if(hotelname==""){
-			return false;
-		}
 
 		//根据酒店名字模糊查询
 		$.ajax({
@@ -76,7 +109,7 @@ $(function() {
 			  },
 			type: 'post',
 			dataType: 'json',
-			data: '{"pageNo":1,"pageSize":10,"hotelname":' + hotelname + '}',
+			data: '{"pageNo":1,"pageSize":10,"hotelname":"' + hotelname + '"}',
 			contentType: 'application/json'
 		})
 		.done(function(data) {
@@ -102,6 +135,55 @@ $(function() {
 				arrList.push(str);
 			}
 			$('#BMOMSWEB').html(arrList.join(''));
+
+			var setTotalCount = data.totalCount;
+			$('#box').paging({
+		        initPageNo: 1, // 初始页码
+		        totalPages: data.totalPages, //总页数
+		        totalCount: '共' + setTotalCount + '条', // 条目总数
+		        slideSpeed: 600, // 缓动速度。单位毫秒
+		        jump: true, //是否支持跳转
+		        callback: function(page) { // 回调函数
+		            console.log("jump to page " + page);
+		            $.ajax({
+	                    url: '/bear/service/hotel/page',
+	                    headers: {
+	                        'Authorization': localStorage.getItem('Authorization') || ''
+	                      },
+	                    type: 'post',
+	                    dataType: 'json',
+						data: '{"pageNo":' + page + ',"pageSize":10,"hotelname":"' + hotelname + '"}',
+	                    contentType: 'application/json'
+	                })
+	                .done(function(data) {
+	                    //console.log(data);
+	                    arr = data.result;
+	                    var arrList = [];
+	                    var head = '<tr>\
+	                                    <th>酒店名称</th><th>酒店房型</th>\
+	                                    <th>酒店早餐</th><th>酒店库存</th>\
+	                                    <th>酒店价格</th><th>酒店价格变化</th>\
+	                                    <th>更新时间</th>\
+	                                </tr>';
+	                    arrList.push(head);
+	                    //有多个应用时，必须列出这些 应用
+	                    for(var i=0; i<arr.length; i++){
+	                        //str += '<li><a data-href="'+arr[i].bizUrl+'" data-guid="'+arr[i].guid+'" href="javascript:;">'+arr[i].name+'</a></li>';
+	                        var str = '<tr>\
+	                                    <td>'+arr[i].hotelname+'</td><td>'+arr[i].fx+'</td>\
+	                                    <td>'+arr[i].zc+'</td><td>'+arr[i].kc+'</td>\
+	                                    <td>'+arr[i].price+'</td><td>'+arr[i].price+'</td>\
+	                                    <td>'+arr[i].updatetime+'</td>\
+	                                   </tr>'
+	                        arrList.push(str);
+	                    }
+	                    $('#BMOMSWEB').html(arrList.join(''));
+	                })
+	                .fail(function() {
+	                    //console.log("error");
+	                });
+		        }
+	    	})
 		})
 		.fail(function() {
 			//console.log("error");
--- alpha-bear/bear-web/src/js/paging.js
+++ alpha-bear/bear-web/src/js/paging.js
@@ -117,6 +117,58 @@
                 that.options.callback(pageIndex);
             }
 
+            function callback(pageIndex) {
+                $.ajax({
+                    url: '/bear/service/hotel/page',
+                    headers: {
+                        'Authorization': localStorage.getItem('Authorization') || ''
+                      },
+                    type: 'post',
+                    dataType: 'json',
+                    data: '{"pageNo":'+ pageIndex + ',"pageSize":10}',
+                    contentType: 'application/json'
+                })
+                .done(function(data) {
+                    //console.log(data);
+                    arr = data.result;
+                    var arrList = [];
+                    var head = '<tr>\
+                                    <th>酒店名称</th><th>酒店房型</th>\
+                                    <th>酒店早餐</th><th>酒店库存</th>\
+                                    <th>酒店价格</th><th>酒店价格变化</th>\
+                                    <th>更新时间</th>\
+                                </tr>';
+                    arrList.push(head);
+                    //有多个应用时，必须列出这些 应用
+                    for(var i=0; i<arr.length; i++){
+                        //str += '<li><a data-href="'+arr[i].bizUrl+'" data-guid="'+arr[i].guid+'" href="javascript:;">'+arr[i].name+'</a></li>';
+                        var str = '<tr>\
+                                    <td>'+arr[i].hotelname+'</td><td>'+arr[i].fx+'</td>\
+                                    <td>'+arr[i].zc+'</td><td>'+arr[i].kc+'</td>\
+                                    <td>'+arr[i].price+'</td><td>'+arr[i].price+'</td>\
+                                    <td>'+arr[i].updatetime+'</td>\
+                                   </tr>'
+                        arrList.push(str);
+                    }
+                    $('#BMOMSWEB').html(arrList.join(''));
+
+                    var setTotalCount = data.totalCount;
+                    $('#box').paging({
+                        initPageNo: 1, // 初始页码
+                        totalPages: data.totalPages, //总页数
+                        totalCount: '共' + setTotalCount + '条', // 条目总数
+                        slideSpeed: 600, // 缓动速度。单位毫秒
+                        jump: true, //是否支持跳转
+                        callback: function(page) { // 回调函数
+                            console.log(page);
+                        }
+                    })
+                })
+                .fail(function() {
+                    //console.log("error");
+                });
+            }
+
             handles(that.options.pageNo); // 初始化页码位置
         }
     }
